dd50932d03340aa1aa79332cf39c8c3869b3f80c
Steve Correa <s.correa@berkeley.edu>
2022-03-09 12:04:08 -0800

    did two sum.

---
+++ easy/1.js
@@ -0,0 +1,53 @@
+var twoSum = function (nums, target) {
+
+    let values = {}
+
+    for (let i = 0; i < nums.length; i++) {
+
+        if (values[nums[i]] !== undefined) {
+
+            let objectValue = values[nums[i]]
+
+            if (Array.isArray(objectValue)) {
+
+                objectValue.push(i)
+
+                console.log("hey")
+
+            } else {
+                let objectValue = values[nums[i]]
+
+                values[nums[i]] = [objectValue]
+                console.log(values[nums[i]])
+
+                values[nums[i]].push(i)
+
+            }
+
+            console.log(values)
+
+        } else {
+            values[nums[i]] = i
+        }
+    }
+
+
+
+    for (let i = 0; i < nums.length; i++) {
+        let compliment = target - nums[i]
+        if (values[compliment]) {
+            if (Array.isArray(values[compliment])) {
+                return values[compliment]
+            }
+            if (compliment !== nums[i]) {
+                return [i, values[compliment]]
+            }
+        }
+    }
+
+}
+
+console.log(twoSum([3, 3], 6))
+
+//time complexity should be 0(n) since we have two for loops not nested. What about hash collisions?
+//space complexity coming soon. 
