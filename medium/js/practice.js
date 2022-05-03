78127d242edc01133d72541c2efe3765e96a7a81
Steve Correa <s.correa@berkeley.edu>
2022-05-03 15:30:39 -0700

    added a practice file for js practice.

---
+++ medium/js/practice.js
@@ -0,0 +1,38 @@
+// let array = [1];
+// let msg = "hey";
+
+// function gg(array, msg) {
+//   array[1] = 2;
+//   msg = "no";
+// }
+
+// gg(array, msg);
+// console.log(array);
+// console.log(msg);
+
+// const x = {};
+// x["foo"] = "bar";
+// x.bar = {
+//   first: 100,
+//   second: 200,
+// };
+
+// console.log(x.bar["first"] + x["bar"].second);
+
+let obj = {
+  1: "one",
+  2: "two",
+};
+
+let message = "no";
+
+const func1 = (obj, msg) => {
+  obj[1] = "hey";
+  msg = "yes";
+  message = 100;
+  console.log(msg);
+  console.log();
+};
+
+func1(obj, message);
+console.log(obj, message);
