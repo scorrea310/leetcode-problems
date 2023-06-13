e7c91630e3e6be8bd69d68d43148eaeab080ee89
Steve Correa <s.correa@berkeley.edu>
2023-06-13 14:28:16 -0700

    Added count-negative-numbers-in-a-sorted-matrix

---
+++ easy/ts/1351.ts
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/description/
+// Time complexity: O(n^2)
+// Space Complexity: O(1)
+var countNegatives = function(grid) {
+    let total = 0
+
+    for(let i = 0; i < grid.length; i++) {
+        for(let j = 0; j < grid[i].length; j++) {
+            if(grid[i][j] < 0) {
+                total++
+            }
+        }
+    }
+
+    return total
+};
