928af8a737dd0c799d2602c36a13bb51839a17e9
Steve Correa <s.correa@berkeley.edu>
2023-06-13 16:52:38 -0700

    Added binary search.

---
+++ easy/ts/704.ts
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/binary-search/description/
+// Time Complexity: O(log(n))
+// Space Complexity: O(1)
+function search(nums: number[], target: number): number {
+    let left = 0
+    let right = nums.length - 1
+    let middle;
+    while(left <= right) {
+        middle = Math.floor(right - left / 2)
+        if(nums[middle] === target) return middle
+        if(nums[middle] > target) {
+            right = middle - 1
+        } else {
+            left = middle + 1
+        }
+    }
+    return -1
+};
