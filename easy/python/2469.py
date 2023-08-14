0f84f5db2d34a5b2b8c719162005eff54c25a0a7
Steve Correa <s.correa@berkeley.edu>
2023-08-14 11:38:13 -0700

    Added python convert-the-temperature.

---
+++ easy/python/2469.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/convert-the-temperature/description/
+# Time and Space: O(1)
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        return [celsius + 273.15, celsius * 1.80 + 32.00]
