4683e0760ebf7020512b34f2965283c95da90665
Steve Correa <s.correa@berkeley.edu>
2023-08-15 09:00:28 -0700

    Added sqrtx.

---
+++ easy/ts/69.ts
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/sqrtx/description/
+// Time: O(log(n))
+// Space: O(1)
+function mySqrt(x: number): number {
+  if (x < 2) return x;
+  let left = 2;
+  let right = Math.floor(x / 2);
+
+  while (left <= right) {
+    let pivot = Math.floor(left + (right - left) / 2);
+    let num = pivot * pivot;
+    if (num > x) {
+      right = pivot - 1;
+    } else if (num < x) {
+      left = pivot + 1;
+    } else {
+      return pivot;
+    }
+  }
+  return right;
+}
