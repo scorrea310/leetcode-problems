3d591ef27bdb217429d95ae7f779069dcc8d2802
Steve Correa <s.correa@berkeley.edu>
2023-09-01 15:23:26 -0700

    Added kth-missing-positive-number log n solution.

--- easy/ts/1539.ts
+++ easy/ts/1539.ts
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/kth-missing-positive-number/description/
-// Time and Space: O(n)
+// Time: O(n)
+// Space: O(1)
 function findKthPositive(arr: number[], k: number): number {
   let numMissing = 0;
   let i = 1;
@@ -16,4 +17,23 @@ function findKthPositive(arr: number[], k: number): number {
     }
   }
   return num;
+}
+
+// Time: log(n)
+//Space: O(1)
+function findKthPositiveLogN(arr: number[], k: number): number {
+  let start: number = 0;
+  let end: number = arr.length - 1;
+
+  while (start <= end) {
+    const middle: number = Math.floor((end - start) / 2) + start;
+
+    if (arr[middle] - (middle + 1) >= k) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+  }
+
+  return start + k;
 }
