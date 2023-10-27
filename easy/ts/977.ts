4b7d0f83a2d2fc765e10313a96039f6d3bfd4311
Steve Correa <s.correa@berkeley.edu>
2023-10-27 15:34:03 -0700

    Added n solution for squares-of-a-sorted-array TS

--- easy/ts/977.ts
+++ easy/ts/977.ts
@@ -2,9 +2,35 @@
 // Time Complexity: O(n * log(n))
 // Space Complexity: O(log(n)) javascript sort()
 function sortedSquares(nums: number[]): number[] {
-    for(let i = 0 ;i < nums.length; i++) {
-        nums[i] = nums[i] * nums[i]
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = nums[i] * nums[i];
+  }
+  nums.sort((a, b) => a - b);
+  return nums;
+}
+
+// https://leetcode.com/problems/squares-of-a-sorted-array/
+// Time and Space: O(n)
+function sortedSquaresN(nums: number[]): number[] {
+  let ansArr = new Array(nums.length).fill(null);
+  let left = 0;
+  let right = nums.length - 1;
+  let end = nums.length - 1;
+  while (left < right) {
+    let leftNum = Math.abs(nums[left] ** 2);
+    let rightNum = Math.abs(nums[right] ** 2);
+    if (rightNum >= leftNum) {
+      ansArr[end] = rightNum;
+      right--;
+    } else {
+      ansArr[end] = leftNum;
+      left++;
     }
-    nums.sort((a, b) => a - b)
-    return nums
-};
+    end--;
+  }
+
+  if (left === right) {
+    ansArr[0] = nums[left] * nums[left];
+  }
+  return ansArr;
+}
